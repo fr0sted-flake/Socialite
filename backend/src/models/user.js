9564fe7bf346e87a58e6fc052e5ee9882ec573b7
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-10-18 23:44:30 +0530

    add user model

---
+++ backend/src/models/user.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png",
+    },
+    isActivated: {
+      type: Boolean,
+      default: true,
+    },
+    deletedAt: {
+      type: Date,
+    },
+    eventRegistered: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Event",
+      },
+    ],
+    eventCreated: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Event",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+export default User;
