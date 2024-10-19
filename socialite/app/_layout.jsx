3d93fa821bda3c7e75bdf6f5b43e25febfac4442
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-10-19 14:28:31 +0530

    auth screen added

--- socialite/app/_layout.jsx
+++ socialite/app/_layout.jsx
@@ -38,8 +38,8 @@ const RootLayout = () => {
   return (
     // <GlobalProvider>
       <Stack>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
         {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
         <Stack.Screen name="event/[eventId]" options={{ headerShown: false }} />
