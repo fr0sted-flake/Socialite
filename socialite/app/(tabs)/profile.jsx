02f862983fd89c59f690e97e61028c8e743970d8
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-10-20 11:07:51 +0530

    profile bug fix

--- socialite/app/(tabs)/profile.jsx
+++ socialite/app/(tabs)/profile.jsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Alert,
   Text,
+  RefreshControl,
 } from "react-native";
 import { images, BASE_URL } from "../../constants";
 import axios from "axios";
@@ -21,6 +22,7 @@ const Profile = () => {
   const [createdEvents, setCreatedEvents] = useState([]);
   const [registeredEvents, setRegisteredEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
   const fetchCreatedEvents = useCallback(async () => {
     try {
@@ -34,142 +36,151 @@ const Profile = () => {
       );
       setCreatedEvents(eventsResponse.data.events);
     } catch (error) {
-      Alert.alert("Error", "Failed to fetch created user events");
+      console.error("Error fetching created events:", error);
+      Alert.alert("Error", "Failed to fetch created events");
     }
   }, [userToken]);
 
   const fetchRegisteredEvents = useCallback(async () => {
     try {
-      const response = await axios.get(`${BASE_URL}/users/registered-events`, {
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-        },
-      });
-      setRegisteredEvents(response.data.events);
+      const eventsResponse = await axios.get(
+        `${BASE_URL}/users/registered-events`,
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+      setRegisteredEvents(eventsResponse.data.events);
     } catch (error) {
+      console.error("Error fetching registered events:", error);
       Alert.alert("Error", "Failed to fetch registered events");
     }
   }, [userToken]);
 
+  const fetchEvents = useCallback(async () => {
+    setIsLoading(true);
+    await Promise.all([fetchCreatedEvents(), fetchRegisteredEvents()]);
+    setIsLoading(false);
+  }, [fetchCreatedEvents, fetchRegisteredEvents]);
+
   useFocusEffect(
     useCallback(() => {
-      setIsLoading(true);
-      fetchCreatedEvents();
-      fetchRegisteredEvents();
-      setIsLoading(false);
-    }, [])
+      fetchEvents();
+    }, [fetchEvents])
   );
 
-  const signout = async () => {
-    try {
-      await logout();
-      Alert.alert("See You Soon", "User logged out successfully");
-      router.replace("/sign-in");
-    } catch (error) {
-      Alert.alert("Error", "Failed to log out");
-    }
-  };
-  const deleteAcc = async () => {
-    try {
-      await axios.delete(`${BASE_URL}/users/delete-user`, {
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-        },
-      });
-      await delAcc();
-      Alert.alert("Bye", "User deleted successfully");
-      router.replace("/sign-in");
-    } catch (error) {
-      Alert.alert("Error", error.message);
-    }
-  };
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchEvents();
+    setRefreshing(false);
+  }, [fetchEvents]);
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <Loader isLoading={isLoading} />
       <FlatList
-        data={createdEvents}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => (
-          <EventCard
-            title={item.title}
-            location={item.eventLocation}
-            eventId={item._id}
-          />
-        )}
-        ListEmptyComponent={() => (
-          <EmptyState
-            title="No Events Found"
-            subtitle="No Events created by you"
-          />
-        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         ListHeaderComponent={() => (
-          <View className="w-full flex justify-center items-center mt-6 mb-12 px-4">
-            <View className="w-full flex justify-center items-center mb-5 px-4">
-              <View className="w-full flex flex-row justify-between">
-                <TouchableOpacity onPress={deleteAcc} className="mb-10">
-                  <Image
-                    source={icons.deleteAccount}
-                    resizeMode="contain"
-                    className="w-6 h-6"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={signout} className="mb-10">
-                  <Image
-                    source={icons.logout}
-                    resizeMode="contain"
-                    className="w-6 h-6"
-                  />
-                </TouchableOpacity>
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Your Profile
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Socialite
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
               </View>
             </View>
+            <View className="w-full items-center justify-center">
+  <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
+    <Image
+      source={images.avatar}
+      className="w-[90%] h-[90%] rounded-lg"
+      resizeMode="cover"
+    />
+  </View>
+</View>
+            <InfoBox title={userInfo?.name} subtitle={userInfo?.email} />
 
-            {userInfo ? (
-              <>
-                <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
-                  <Image
-                    source={images.avatar}
-                    className="w-[90%] h-[90%] rounded-lg"
-                    resizeMode="cover"
-                  />
-                </View>
-
-                <InfoBox
-                  title={userInfo.name}
-                  containerStyles="mt-5"
-                  titleStyles="text-lg"
+            <View className="flex-row justify-between">
+              <TouchableOpacity
+                className="flex-row items-center bg-black-100 px-4 py-2 rounded-xl"
+                onPress={logout}
+              >
+                <Image
+                  source={icons.logout}
+                  className="w-5 h-5 mr-2"
+                  resizeMode="contain"
                 />
+                <Text className="text-white">Logout</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="flex-row items-center bg-red-500 px-4 py-2 rounded-xl"
+                onPress={delAcc}
+              >
+                <Image
+                  source={icons.deleteAccount}
+                  className="w-5 h-5 mr-2"
+                  resizeMode="contain"
+                />
+                <Text className="text-white">Delete Account</Text>
+              </TouchableOpacity>
+            </View>
 
-                <View className="mt-5 flex flex-row">
-                  <InfoBox
-                    title={createdEvents.length || "0"}
-                    subtitle="Events Created"
-                    titleStyles="text-xl"
-                    containerStyles="mr-10"
-                  />
-                  <InfoBox
-                    title={registeredEvents.length || "0"}
-                    subtitle="Events Registered"
-                    titleStyles="text-xl"
-                  />
-                </View>
-              </>
-            ) : (
-              <Text className="text-white">No user Info found</Text>
-            )}
-            <View className="w-full flex-1 pt-10 ">
-              <Text className="text-lg font-pregular text-gray-100">
+            <View className="w-full flex-1 pt-5">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
                 Created Events
               </Text>
+              {createdEvents.length > 0 ? (
+                createdEvents.map((event) => (
+                  <EventCard
+                    key={event._id}
+                    title={event.title}
+                    location={event.eventLocation}
+                    eventId={event._id}
+                  />
+                ))
+              ) : (
+                <EmptyState
+                  title="No Created Events"
+                  subtitle="You haven't created any events yet"
+                />
+              )}
+            </View>
+
+            <View className="w-full flex-1 pt-5">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Registered Events
+              </Text>
+              {registeredEvents.length > 0 ? (
+                registeredEvents.map((event) => (
+                  <EventCard
+                    key={event._id}
+                    title={event.title}
+                    location={event.eventLocation}
+                    eventId={event._id}
+                  />
+                ))
+              ) : (
+                <EmptyState
+                  title="No Registered Events"
+                  subtitle="You haven't registered for any events yet"
+                />
+              )}
             </View>
           </View>
         )}
-        refreshing={isLoading}
-        onRefresh={() => {
-          fetchRegisteredEvents();
-          fetchCreatedEvents()
-          return
-        }}
       />
     </SafeAreaView>
   );
